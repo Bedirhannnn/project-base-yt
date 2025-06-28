5c03a348ff1a0654e911c469fcc37f8411ff927a
Bedirhannnn <bedirhanyiber@gmail.com>
2025-06-28 16:47:59 +0300

    ufak bi silme islemi gerksiz commit

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const isAuthenticated = true;
-
-router.all("*", (req, res, next) => {
-    if (isAuthenticated) {
-        next();
-    } else {
-        res.json({ success: false, error: "Not authenticated" });
-    }
-})
 
 
 
-/* GET users listing. */
+
+/* GET categories listing. */
 router.get('/', function (req, res, next) {
     res.json({ success: true });
 });
