e848fe4b7080ffb39dcb8e534d1e3fe37a128be4
Bedirhannnn <bedirhanyiber@gmail.com>
2025-07-16 23:36:20 +0300

    user auth yapısı kuruldu

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,15 @@
 var express = require('express');
-const bcrypt = require("bcrypt-nodejs")
+const bcrypt = require("bcrypt")
 const is = require("is_js");
+const jwt = require("jwt-simple");
+
 const Users = require('../db/models/Users');
 const Response = require("../lib/Response");
 const CustomError = require('../lib/Error');
 const Enum = require('../config/Enum');
 const UserRoles = require('../db/models/UserRoles');
 const Roles = require("../db/models/Roles");
+const config = require("../config");
 var router = express.Router();
 
 /* GET users listing. */
@@ -195,5 +198,47 @@ router.post("/register", async (req, res) => {
     res.status(errorResponse.code).json(errorResponse);
   }
 });
+
+router.post("/auth", async (req, res) => {
+  try {
+
+    let { email, password } = req.body;
+
+    console.log("Giriş yapan email:", email);
+    console.log("Giriş yapan şifre:", password);
+
+    Users.validateFieldsBeforeAuth(email, password);
+
+    let user = await Users.findOne({ email });
+    console.log("MongoDB'den gelen user:", user);
+    if (!user) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Email or password wrong");
+
+    if (!user.validPassword(password)) {
+      console.log("Şifre doğrulama başarısız.");
+      throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Email or password wrong");
+    }
+
+    let payload = {
+      id: user._id,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60
+      //exp: parseInt(Date.now() / 1000) * config.JWT.EXPIRE_TIME
+    }
+
+    let token = jwt.encode(payload, config.JWT.SECRET);
+
+    let userData = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name
+    }
+
+    res.json(Response.successResponse({ token, user: userData }));
+
+  } catch (err) {
+    console.error("Hata:", err);
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+})
 
 module.exports = router;
